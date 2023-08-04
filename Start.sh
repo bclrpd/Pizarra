7252d6ab1d3bd55665668436287ee145ef35e63d
bclrpd <78045587+bclrpd@users.noreply.github.com>
2023-08-04 16:56:00 -0400

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -3,7 +3,9 @@ cd "$(dirname "$0")"
 until ping -nq -c3 8.8.8.8; do
 	sleep 1
 done
-sleep 30
+sleep 10
+bash UpdateChek.sh
+sleep 20
 wget https://drive.google.com/u/0/uc?id=1xoK8xsJ014ijRmCHnP4ZNjyv_NTI7thh -O Premios.ini 
 python3 main.py &
 xdotool mousemove 10000 10000
