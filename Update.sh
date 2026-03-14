7a513f3f0dab4e26998438d7eaf39caae7b8f8f8
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-03-14 19:23:59 -0400

    Refactor Update.sh to use temporary directory
    Update script to download files into a temporary directory and check file size before copying.

--- Update.sh
+++ Update.sh
@@ -5,27 +5,29 @@ chmod +x /home/ventas/.Auto/chromium-browser
 
 URL=https://raw.githubusercontent.com/bclrpd/Pizarra/main/
 Archivo=(UpdateChek.sh main.py Start.sh PizarraWeb.py)
-Imagenes=(IMG/ANG10am.png IMG/ANG1pm.png IMG/ANG6pm.png IMG/ANG9pm.png)
 
 X=0
 
-for i in "${Archivo[@]}"
-do
+for i in "${Archivo[@]}"; do
 	wget -q --method HEAD $URL$i
 	if [ $? -eq 0 ] ; then
-		curl -sSL $URL$i | tr -d '\r' >$i
+		curl -sSL $URL$i | tr -d '\r' >tmp/$i
 		[ $? -eq 0 ] || X=1 
 	fi
 done
 
-#for i in "${Imagenes[@]}"
-#do
-	#wget -q --method HEAD $URL$i
-	#if [ $? -eq 0 ] ; then
-		#wget $URL$i -q -O- >$i
-		#[ $? -eq 0 ] || X=1 
-	#fi
-#done
+for i in "${Archivo[@]}"; do
+    if [ $(stat -c%s tmp/$i) -gt 100 ] ; then
+        cp -f tmp/$i /home/ventas/.Auto/Pizarra/$i
+        if [ $? -eq 0 ]; then
+			rm tmp/$i
+        else
+             X=1
+		fi  
+    else
+        X=1 
+    fi        
+done
 
 
 [ $X -eq 0 ] && echo "Version=$1" > Current.ini && rm Update.sh
