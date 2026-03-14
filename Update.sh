301b6276ce16dfdba1d6179aa9c5939a2359564d
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-03-14 19:15:49 -0400

    Update file download method and modify reboot command

--- Update.sh
+++ Update.sh
@@ -4,7 +4,7 @@ chmod +x /home/ventas/.Auto/chromedriver
 chmod +x /home/ventas/.Auto/chromium-browser
 
 URL=https://raw.githubusercontent.com/bclrpd/Pizarra/main/
-Archivo=(main.py Start.sh PizarraWeb.py)
+Archivo=(UpdateChek.sh main.py Start.sh PizarraWeb.py)
 Imagenes=(IMG/ANG10am.png IMG/ANG1pm.png IMG/ANG6pm.png IMG/ANG9pm.png)
 
 X=0
@@ -13,7 +13,7 @@ for i in "${Archivo[@]}"
 do
 	wget -q --method HEAD $URL$i
 	if [ $? -eq 0 ] ; then
-		wget $URL$i -q -O- | tr -d '\r' >$i
+		curl -sSL $URL$i | tr -d '\r' >$i
 		[ $? -eq 0 ] || X=1 
 	fi
 done
@@ -31,7 +31,7 @@ done
 [ $X -eq 0 ] && echo "Version=$1" > Current.ini && rm Update.sh
 
 sleep 1
-systemctl reboot -i
+#systemctl reboot -i
 #--------------------
 
 exit
