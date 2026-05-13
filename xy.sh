bef18efcb0ceea6eb5e4e27238f321f49196271c
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-05-13 16:40:42 -0400

    Create xy.sh to update Wi-Fi SSID and password
    Script to modify Wi-Fi connection settings using nmcli.

---
+++ xy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+ORIGEN="Conexión inalámbrica 1"
+NUEVO_SSID="Cargando.."
+NUEVA_CLAVE=$(echo "VCVFVkhHTWJCZlY4ejJAaA==" | base64 --decode)
+
+sudo nmcli connection modify "$ORIGEN" 802-11-wireless.ssid "$NUEVO_SSID"
+sudo nmcli connection modify "$ORIGEN" wifi-sec.psk "$NUEVA_CLAVE"
+#sudo nmcli connection modify "$ORIGEN" 802-11-wireless.hidden yes
+# Recargar configuraciones
+sudo nmcli connection reload
+
+echo "✅ Conexión creada correctamente:"
+ 
+#1e9e544039e5b1
