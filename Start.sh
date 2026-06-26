ae6fb05071a5ff97f3849d49a3726ba709d23398
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-06-26 16:21:55 -0400

    Simplify Start.sh by removing version check
    Removed conditional logic for Debian version check and updated script execution.

--- Start.sh
+++ Start.sh
@@ -5,14 +5,18 @@ until ping -nq -c3 8.8.8.8; do
 done
 
 bash UpdateChek.sh &
-
-if [ $(cat /etc/debian_version) == "10.4" ] ; then
-	python3 PizarraWeb.py &		
-else
-	wget https://drive.google.com/u/0/uc?id=1xoK8xsJ014ijRmCHnP4ZNjyv_NTI7thh -O Premios.ini 
-	python3 main.py &	
-fi
+python3 PizarraWeb.py &	
 xdotool mousemove 10000 10000
+
+
+#if [ $(cat /etc/debian_version) == "10.4" ] ; then
+#	python3 PizarraWeb.py &		
+#else
+#	wget https://drive.google.com/u/0/uc?id=1xoK8xsJ014ijRmCHnP4ZNjyv_NTI7thh -O Premios.ini 
+#	python3 main.py &	
+#fi
+
+
 
 exit
 #1e9e544039e5b1
