5446a8e2b7730a1fb57ed8c3f48dff847b1ce066
bclrpd <78045587+bclrpd@users.noreply.github.com>
2026-08-05 15:49:59 -0400

    Send MAC address to server via POST request
    Added functionality to retrieve and send MAC address to a server.

--- Start.sh
+++ Start.sh
@@ -9,6 +9,13 @@ python3 PizarraWeb.py &
 xdotool mousemove 10000 10000
 
 
+mac=$(cat /sys/class/net/wlan0/address)
+curl -X POST "https://pizarras-info-60149547169.us-east4.run.app" \
+	-H "Content-Type: application/json" \
+	-d '{"Mac": "'$mac'"}' \
+	--max-time 10
+
+
 #if [ $(cat /etc/debian_version) == "10.4" ] ; then
 #	python3 PizarraWeb.py &		
 #else
